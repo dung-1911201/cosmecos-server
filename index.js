307641d33eec0cb3ec5762457ff85a87b4e59b0b
dungkieu002 <dungkieu002@gmail.com>
2023-08-02 14:06:01 +0700

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const commentRouter = require('./src/routers/commentRouter');
 const orderRouter = require('./src/routers/orderRouter');
 
 mongoose
-	.connect(process.env.MONGO_URL)
+	.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => console.log('db is connected !'))
 	.catch((err) => console.log(err));
 
