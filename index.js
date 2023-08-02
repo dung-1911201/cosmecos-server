2557bd74dc7102402a6b4b6246c281ac264d8b85
dungkieu002 <dungkieu002@gmail.com>
2023-08-02 18:17:26 +0700

    .

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ mongoose
 app.use(express.json());
 //PORT
 
-// // Tuyến đường xử lý yêu cầu đến địa chỉ gốc "/"
-// app.get('/', (req, res) => {
-// 	res.send('Xin chào, đây là trang chủ của ứng dụng!');
-// });
+// Tuyến đường xử lý yêu cầu đến địa chỉ gốc "/"
+app.get('/', (req, res) => {
+	res.send('Xin chào, đây là trang chủ của ứng dụng!');
+});
 
 const PORT = process.env.PORT || 5050;
 
